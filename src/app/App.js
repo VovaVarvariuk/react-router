26f1dba409a457f4e6f2d798fdbd976d460d525c
VovaVarvariuk <78534851+VovaVarvariuk@users.noreply.github.com>
2021-11-03 23:12:09 +0200

    update of reaect-router

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,11 @@
 import NavBar from "../components/navBar";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Dashboard from "../components/dashboard";
 import Login from "../components/login";
-// import Posts from "../components/posts";
+import Posts from "../components/posts";
 import Home from "../components/home";
-import PostsList from "../components/postsList";
-import Post from "../components/post";
-
-const posts = [
-  { id: 1, label: "post 1" },
-  { id: 2, label: "post 2" },
-  { id: 3, label: "post 3" },
-];
+import Stats from "../components/stats";
+import NotFound from "../components/not-found";
 
 function App() {
   return (
@@ -19,21 +13,18 @@ function App() {
       <NavBar />
       <h1>App</h1>
       <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/dashboard/stats" component={Stats} />
         <Route
           path="/dashboard"
           component={Dashboard}
           //   render={(props) => <Dashboard isAdmin={false} {...props} />}
         />
         <Route path="/login" component={Login} />
-        <Route
-          path="/posts/:postId"
-          render={(props) => <Post posts={posts} {...props} />}
-        />
-        <Route
-          path="/posts"
-          render={(props) => <PostsList posts={posts} {...props} />}
-        />
-        <Route path="/" component={Home} />
+        <Route path="/posts/:postId?" component={Posts} />
+        <Route path="/404" component={NotFound} />
+        <Redirect from="/admin" to="/dashboard" />
+        <Redirect to="/404" />
       </Switch>
     </div>
   );
--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,5 +1,29 @@
-const Posts = () => {
-  return <h1>Posts</h1>;
+import PostsList from "./postsList";
+import Post from "./post";
+import query from "query-string";
+import _ from "lodash";
+
+const Posts = ({ match, location }) => {
+  const posts = [
+    { id: 1, label: "post 1" },
+    { id: 2, label: "post 2" },
+    { id: 3, label: "post 3" },
+  ];
+  const search = query.parse(location.search);
+  const postId = match.params.postId;
+  const cropPosts = search
+    ? _(posts).slice(0).take(search.count).value()
+    : posts;
+
+  return (
+    <>
+      {postId ? (
+        <Post id={postId} posts={posts} />
+      ) : (
+        <PostsList posts={cropPosts} />
+      )}
+    </>
+  );
 };
 
 export default Posts;
